728989aed134fd62451cf2cdf7cbaf95ca463f7c
Paul Maxwell <165905426+pmaxunlimited@users.noreply.github.com>
2024-07-07 14:21:49 -0400

    Restructure pages

---
+++ app/_components/Counter.js
@@ -0,0 +1,13 @@
+"use client";
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+  console.log(users);
+  return (
+    <>
+      <p>There are {users.length} users</p>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </>
+  );
+}
