fe7bdcf0323223b214b2b42516062911ab9f0767
Paul Maxwell <165905426+pmaxunlimited@users.noreply.github.com>
2024-07-07 15:04:13 -0400

    Add image optimization

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import { Josefin_Sans } from "next/font/google";
-Josefin_Sans({
+
+const josefin = Josefin_Sans({
   subsets: ["latin"],
-  display: "swap",
+  display: "swap", // for variable weight fonts
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -20,14 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-
-        <main> {children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={` ${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className=" max-w-7xl mx-auto"> {children}</main>
+        </div>
       </body>
     </html>
   );
