1cb06e9f00e6c1ef6798f821301765a7f8594a19
Paul Maxwell <165905426+pmaxunlimited@users.noreply.github.com>
2024-07-14 12:01:39 -0400

    Add static site regeneration and data revalidation to dynamic pages

--- app/cabins/[cabinid]/page.js
+++ app/cabins/[cabinid]/page.js
@@ -1,35 +1,42 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
-// PLACEHOLDER DATA
-// const cabin = {
-//   id: 89,
-//   name: "001",
-//   maxCapacity: 2,
-//   regularPrice: 250,
-//   discount: 0,
-//   description:
-//     "Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.",
-//   image:
-//     "https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg",
+// export const metadata = {
+//   title: "Cabin",
 // };
+
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinid);
+  return { title: `Cabin ${name}` };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  const ids = cabins.map((cabin) => ({
+    cabinid: String(cabin.id),
+  }));
+
+  console.log(ids);
+
+  return ids;
+}
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinid);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
-  console.log(params.cabinid);
-  console.log(params);
-  console.log(cabin);
-
-  //   console.log(params);
-
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-        <div className="relative scale-[1.15] -translate-x-3">
-          <img src={image} alt={`Cabin ${name}`} />
+        <div className="relative scale-[1.15] -translate-x-3 ">
+          <Image
+            src={image}
+            fill
+            className="object-cover"
+            alt={`Cabin ${name}`}
+          />
         </div>
 
         <div>
