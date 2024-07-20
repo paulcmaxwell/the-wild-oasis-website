fbe462f0a0afc81adab4f6c9f33a693a9d2b4d3b
Paul Maxwell <paulcmaxwell@gmail.com>
2024-07-20 11:35:40 -0400

    Add OAuth with NextAuth

---
+++ app/api/cabins/[cabinid]/route.js
@@ -0,0 +1,17 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinid } = params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinid),
+      getBookedDatesByCabinId(cabinid),
+    ]);
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
+
+// export async function POST() {}
