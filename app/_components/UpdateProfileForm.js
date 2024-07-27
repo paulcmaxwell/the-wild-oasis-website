8f66d3699de04eac3770e3f72bd63d386bc8a177
Paul Maxwell <paulcmaxwell@gmail.com>
2024-07-27 10:53:02 -0400

    Add cache revalidation to booking update form

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { useFormStatus } from "react-dom";
 import { updateProfile } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
 
 function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
@@ -54,21 +54,10 @@ function UpdateProfileForm({ guest, children }) {
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-      <Button />
-      <div className="flex justify-end items-center gap-6"></div>
+      <div className="flex justify-end items-center gap-6">
+        <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
+      </div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending, formData, method } = useFormStatus();
-  return (
-    <button
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-      disabled={pending}
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
